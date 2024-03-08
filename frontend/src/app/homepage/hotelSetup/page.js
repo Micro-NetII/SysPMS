5b00e5ea47a19eac807a3021aca9494d4aed84d5
Bruno Moreira <kayunowna@gmail.com>
2024-03-08 16:23:33 +0000

    Add cards to hotel setup page

--- frontend/src/app/homepage/hotelSetup/page.js
+++ frontend/src/app/homepage/hotelSetup/page.js
@@ -1,10 +1,32 @@
 "use client"
-import Loader from "@/components/Loader/Loader"
+import Cartao from "@/components/Cards/Card";
+import { PiListPlus } from "react-icons/pi";
+import { FaBoxesStacked } from "react-icons/fa6";
+import { FaBed } from "react-icons/fa";
+import { FaWrench } from "react-icons/fa";
+import { FaPencilRuler } from "react-icons/fa";
 
 const HotelSetup = () => {
     return (
         <>
-            <Loader/>
+        <div className="border grid grid-cols-4 gap-4 justify-between py-5 px-5">
+            <div className="">
+                <Cartao title={"GRUPOS TIPOLOGIAS"} description={"Inserir grupos que relacionam várias tipologias de quartos"} icon={<PiListPlus size={35}/>}/>
+            </div>
+            <div className="">
+                <Cartao title={"TIPOLOGIAS"} description={"Inserir as tipologias que associam quartos da mesma categoria"} icon={<FaBoxesStacked size={35}/>}/>
+            </div>
+            <div className="">
+                <Cartao title={"QUARTOS"} counter1={"39"} counter2={"01"} icon={<FaBed size={35}/>}/>
+            </div>
+            <div className="">
+                <Cartao title={"CARACTERÍSTICAS"} description={"Inserir lista com as várias características para cada quarto"} icon={<FaPencilRuler size={35}/>}/>
+            </div>
+            <div className="">
+                <Cartao title={"MANUTENÇÃO"} description={""} icon={<FaWrench size={35}/>}/>
+            </div>
+
+        </div>
         </>
     )
 }
